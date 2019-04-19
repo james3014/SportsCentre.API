cba4d44873b57934bb4c67622b470f4dacaddec9
james3014 <grantj3014@gmail.com>
2019-04-19 06:35:23 +0100

    Repository pattern and register method

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SportsCentre.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
