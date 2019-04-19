04064d351fee79a0a33bd17b63fb31091f9fbff6
james3014 <grantj3014@gmail.com>
2019-04-19 06:46:32 +0100

    Password hash, salt and login method created

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SportsCentre.API.Models;
 
 namespace SportsCentre.API.Data
@@ -13,9 +14,30 @@ namespace SportsCentre.API.Data
             this.context = context;
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string email, string password)
         {
-            throw new System.NotImplementedException();
+            User user = await context.Users.FirstOrDefaultAsync(x => x.Email == email);
+
+            if (user == null) return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) return null;
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
@@ -38,13 +60,14 @@ namespace SportsCentre.API.Data
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-            
+            }  
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string email)
         {
-            throw new System.NotImplementedException();
+            if (await context.Users.AnyAsync(x => x.Email == email)) return true;
+
+            return false;
         }
     }
 }
